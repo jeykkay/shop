246d5b3096c73def67e4998b7b0bc252e6c6df84
user <evgeniy.sumar.smr@gmail.com>
2024-04-01 21:33:25 +0300

    catalog endpoints

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from catalog.views import CategoryListView, CategoryProductsView, SellerProductsView, SellerListView, DiscountListView, DiscountsView
+
+urlpatterns = [
+    path('categories/', CategoryListView.as_view(), name='categories'),
+    path('categories/<int:category_id>/', CategoryProductsView.as_view(), name='category-products'),
+    path('sellers/', SellerListView.as_view(), name='seller'),
+    path('sellers/<int:seller_id>/', SellerProductsView.as_view(), name='seller-products'),
+    path('discounts/', DiscountListView.as_view(), name='discounts'),
+    path('discounts/<int:discount_id>/', DiscountsView.as_view(), name='discount-products'),
+]
