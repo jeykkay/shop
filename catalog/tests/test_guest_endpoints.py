7c202e4834e034c64ec6fe46ccbd4394192bd256
user <evgeniy.sumar.smr@gmail.com>
2024-04-03 21:57:37 +0300

    test guest endpoints

--- catalog/tests/test_guest_endpoints.py
+++ catalog/tests/test_guest_endpoints.py
@@ -1,10 +1,39 @@
 import pytest
 from rest_framework.test import APITestCase
+from django.shortcuts import reverse
+from conftest import EVERYTHING_EQUALS_NOT_NONE
 
 pytestmark = [pytest.mark.django_db]
 
 
 class TestGuestEndpoints(APITestCase):
+    fixtures = ['catalog/tests/fixtures/categories_fixture.json']
 
-    def test_something(self):
-        assert True == True
+    def test_categories_list_endpoint(self):
+        url = reverse('categories')
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert isinstance(response.data, list)
+        assert response.data == [
+            {
+                "id": 1,
+                "name": EVERYTHING_EQUALS_NOT_NONE,
+                "description": EVERYTHING_EQUALS_NOT_NONE
+            },
+            {
+                "id": 2,
+                "name": EVERYTHING_EQUALS_NOT_NONE,
+                "description": EVERYTHING_EQUALS_NOT_NONE
+            },
+            {
+                "id": 3,
+                "name": EVERYTHING_EQUALS_NOT_NONE,
+                "description": EVERYTHING_EQUALS_NOT_NONE
+            },
+            {
+                "id": 4,
+                "name": EVERYTHING_EQUALS_NOT_NONE,
+                "description": EVERYTHING_EQUALS_NOT_NONE
+            }
+
+        ]
