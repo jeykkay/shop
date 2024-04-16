838385b6d39f8ac1d19ce09fe879d3e107ab6678
admin <evgeniy.sumar.smr@gmail.com>
2024-04-16 17:22:28 +0300

    tasks

--- shop/settings.py
+++ shop/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'users.apps.UsersConfig',
     'catalog.apps.CatalogConfig',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -188,3 +189,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = f'redis://{env("CELERY_HOST")}:{env("CELERY_PORT")}'
+CELERY_RESULT_BACKEND = f'redis://{env("CELERY_HOST")}:{env("CELERY_PORT")}'
